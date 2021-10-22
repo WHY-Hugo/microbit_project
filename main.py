7dee8db165605d5be3a4ab798f8e873273039d10
WHY Wang <75600289+WHY-Hugo@users.noreply.github.com>
2021-10-22 15:09:06 +0800

    test

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 def on_forever():
-    basic.show_number(mooncar.line_follower_sensor())
+    if mooncar.ultrasonic_sensor() < 30:
+        mooncar.moon_car_go(mooncar.Direction.DIRECT1, 0)
+    else:
+        mooncar.moon_car_go(mooncar.Direction.DIRECT1, 30)
 basic.forever(on_forever)
+
+def on_forever2():
+    mooncar.filllight(mooncar.Switch.OPEN)
+basic.forever(on_forever2)
+
+def on_forever3():
+    if mooncar.line_follower_sensor() == 0:
+        mooncar.moon_car_go(mooncar.Direction.DIRECT1, 30)
+    elif mooncar.line_follower_sensor() == 1:
+        mooncar.moon_car_lr(0, 15)
+    elif mooncar.line_follower_sensor() == 2:
+        mooncar.moon_car_lr(15, 0)
+    else:
+        pass
+basic.forever(on_forever3)
+
+def on_forever4():
+    basic.show_number(mooncar.line_follower_sensor())
+basic.forever(on_forever4)
--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
 basic.forever(function () {
     basic.showNumber(mooncar.LineFollowerSensor())
 })
+basic.forever(function () {
+    if (mooncar.UltrasonicSensor() < 30) {
+        mooncar.MoonCarGo(mooncar.Direction.direct1, 0)
+    } else {
+        mooncar.MoonCarGo(mooncar.Direction.direct1, 30)
+    }
+})
+basic.forever(function () {
+    mooncar.Filllight(mooncar.Switch.Open)
+})
+basic.forever(function () {
+    if (mooncar.LineFollowerSensor() == 0) {
+        mooncar.MoonCarGo(mooncar.Direction.direct1, 30)
+    } else if (mooncar.LineFollowerSensor() == 1) {
+        mooncar.MoonCarLR(0, 15)
+    } else if (mooncar.LineFollowerSensor() == 2) {
+        mooncar.MoonCarLR(15, 0)
+    } else {
+    	
+    }
+})
